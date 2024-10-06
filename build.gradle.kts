2a47408ad8b61b6c2e46f2114115106f97d25a20
radoslaw.panuszewski <radoslaw.panuszewski@allegro.com>
2024-10-06 08:27:38 +0200

    Migrate to intellij-platform plugin 2.0, bump Gradle and Kotlin, use axion-release

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,46 +2,79 @@
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.intellij)
+    alias(libs.plugins.intellij.platform)
+    alias(libs.plugins.axion.release)
 }
-
-group = "pl.semidude"
-version = versionFromNearestTag()
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://www.jetbrains.com/intellij-repository/snapshots/") }
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-intellij {
-    version = "242-EAP-SNAPSHOT"
-    type = "IU"
-    plugins = listOf("DatabaseTools")
-    updateSinceUntilBuild = false
+scmVersion {
+    tag {
+        fallbackPrefixes = listOf("")
+    }
+    unshallowRepoOnCI = true
 }
 
-java {
-    toolchain {
+group = "pl.semidude"
+version = scmVersion.version
+
+kotlin {
+    jvmToolchain {
         languageVersion = JavaLanguageVersion.of(17)
     }
 }
 
-tasks {
-    runIde {
-        ideDir = File("${System.getenv("HOME")}/Applications/DataGrip.app/Contents")
+dependencies {
+    intellijPlatform {
+        intellijIdeaUltimate("2024.2.3")
+        bundledPlugins("com.intellij.database")
+        instrumentationTools()
     }
+}
 
-    signPlugin {
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "242"
+            untilBuild = provider { null }
+        }
+        vendor {
+            name = "Radosław Panuszewski"
+            email = "radoslaw.panuszewski15@gmail.com"
+            url = "https://panuszewski.dev"
+        }
+        id = "pl.semidude.tree-view-by-default"
+        name = "Tree View By Default"
+        description = """
+            If you use document databases (like MongoDB), it is more convenient to preview the query
+            results in <tt>Tree</tt> mode instead of the standard <tt>Table</tt> mode. Unfortunately, DataGrip does
+            not provide any way to set <tt>Tree<tt> as default presentation mode, so you have to switch it
+            manually every time.
+            <br /> <br />
+            This plugin comes to rescue!
+            <br /> <br />
+            What it does:
+            <ul>
+                <li>Changes default presentation mode of the query results to <tt>Tree</tt></li>
+                <li>If there is only one result, it is automatically expanded</li>
+                <li>You can copy value of the leaf node without the containing JSON via Cmd+Option+C </li>
+            </ul>
+            <br />
+            It is recommended to use this plugin in DataGrip, but it should also work in other IDEs with database support.
+        """.trimIndent()
+    }
+    signing {
         certificateChain = System.getenv("CERTIFICATE_CHAIN")
         privateKey = System.getenv("PRIVATE_KEY")
         password = System.getenv("PRIVATE_KEY_PASSWORD")
     }
-
-    publishPlugin {
+    publishing {
         token = System.getenv("PUBLISH_TOKEN")
     }
-
-    buildSearchableOptions {
-        enabled = false
-    }
+    buildSearchableOptions = false
 }
--- buildSrc/src/main/kotlin/ProjectVersionUtils.kt
+++
@@ -1,4 +0,0 @@
-import org.gradle.api.Project
-
-fun Project.versionFromNearestTag(): String =
-    exec("git describe --tags --always --first-parent").trim()
